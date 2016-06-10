ea30a84e2b366423fab3b77ade3b0ae4c7a52961
Sergey Kostyrko <srg.kostyrko@gmail.com>
2016-06-10 23:05:32 +0300

    implement deepAssign

--- week03/lec1/index.js
+++ week03/lec1/index.js
@@ -8,7 +8,24 @@ const toObject = (arg) => {
   }
 
   return to;
-}
+};
+const isPrimitive = (value) => value !== Object(value);
+const propertyIsEnumerable = {}.propertyIsEnumerable;
+const hasOwnProperty = {}.hasOwnProperty;
+
+const mergeKey = (to,  from) => {
+  if (isPrimitive(from)) {
+    return from;
+  } else if (isPrimitive(to)) {
+    if (from instanceof Date || from instanceof RegExp || from instanceof Set || from instanceof Map) {
+      return new from.constructor(from);
+    } else {
+      return deepAssign({}, from);
+    }
+  } else {
+    return deepAssign(to, from);
+  }
+};
 
 const deepAssign = (target, ...sources) => {
   if (target == null) {
@@ -20,11 +37,16 @@ const deepAssign = (target, ...sources) => {
     if (source == null) return;
     let from = toObject(source);
     Reflect.ownKeys(from).forEach((key) => {
-      if (!from.propertyIsEnumerable(key)) return;
+      if (!propertyIsEnumerable.call(from, key)) return;
+      if (hasOwnProperty.call(to,  key)) {
+        to[key] = mergeKey(to[key], from[key]);
+      } else {
+        to[key] = mergeKey(undefined, from[key]);
+      }
     });
   });
 
   return to;
-}
+};
 
 module.exports = deepAssign;
--- week03/lec1/test.js
+++ week03/lec1/test.js
@@ -24,19 +24,124 @@ tape('processing target', (t) => {
   t.end();
 });
 
-tape('processing sources', (t) => {
+tape('skip non enumerable props', (t) => {
+  let target = {};
+  let source = {};
+  Object.defineProperty(source, 'test', {
+    enumerable: false
+  });
+  let result = deepAssign(target, source);
+  t.notOk(('test' in result), ' ' );
 
-  tape(' ', (st) => {
-    let target = {};
-    let source = {};
-    Object.defineProperty(source, 'test', {
-      enumerable: false,
-    });
-    let result = deepAssign(target, source);
-    t.notOk(('test' in target), 'skip non enumerable props');
+  t.end();
+});
 
-    st.end();
-  })
+tape('key order should be preserved', (t) => {
+  let target = {};
+  let source = {};
+  source.c = true;
+  source.a = true;
+  let result = deepAssign(target, source);
+  t.deepEquals(Object.keys(result), ['c', 'a'], ' ');
+  t.end();
+});
 
+tape('it adds new properties', (t) => {
+  let target = {};
+  let source = {};
+  source.a = true;
+  let result = deepAssign(target, source);
+  t.ok(result.a, ' ');
+  t.end();
+});
+
+tape('extends objects with new props', (t) => {
+  let target = {
+    a: {
+      d: true
+    }
+  };
+  let source = {
+    a: {
+      c: 2
+    }
+  };
+  let result = deepAssign(target, source);
+  t.deepEquals(result, {a: {c: 2, d: true}},  ' ');
+  t.end();
+});
+
+tape('it overwrites properties with primitive values', (t) => {
+  let target = {
+    a: {
+      d: true
+    }
+  };
+  let source = {
+    a: true
+  };
+  let result = deepAssign(target, source);
+  t.ok(result.a, ' ');
+  t.end();
+});
+
+tape('clones Date objects', (t) => {
+  const date = new Date();
+  let target = {};
+  let source = {
+    a: date
+  };
+  let result = deepAssign(target, source);
+  t.ok((result.a instanceof Date), 'date object is created');
+  t.notEquals(result.a, source.a, 'date object is different');
+  t.equals(result.a.getTime(), source.a.getTime(), 'date object has same timestamp');
+  t.end();
+});
+
+tape('clones RegExp objects', (t) => {
+  const rx = new RegExp('.*?', 'i');
+  let target = {};
+  let source = {
+    a: rx
+  };
+  let result = deepAssign(target, source);
+  t.ok((result.a instanceof RegExp), 'RegExp object is created');
+  t.notEquals(result.a, source.a, 'RegExp object is different');
+  t.equals(result.a.toString(), source.a.toString(), 'RegExp object has same pattern');
+  t.end();
+});
+
+tape('works with subclasses of native objects', (t) => {
+  class MyDate extends Date {}
+  let target = {};
+  let source = {
+    a: new MyDate()
+  };
+  let result = deepAssign(target, source);
+  t.ok((result.a instanceof MyDate), ' ');
+  t.end();
+});
+
+tape('works with objects created with Object.crete(null)', (t) => {
+  let target = Object.create(null);
+  let source = Object.create(null);
+  source.a = true;
+  let result = deepAssign(target, source);
+  t.ok(result.a, ' ');
+  t.end();
+});
+
+tape('works with gettters', (t) => {
+  let target = {};
+  let source = {
+    _x: true,
+    get x() {
+      return this._x;
+    }
+  };
+  console.log(source);
+  let result = deepAssign(target, source);
+  console.log(result);
+  t.ok(result.x, ' ');
   t.end();
 });
