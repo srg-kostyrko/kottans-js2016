3f6d48e50100f4a2dab8b133d32b7db7b7bc2f57
Sergey Kostyrko <srg.kostyrko@gmail.com>
2016-06-13 18:53:06 +0300

    implement ExtendedPromise.some

--- week04/lec01/index.js
+++ week04/lec01/index.js
@@ -1,12 +1,64 @@
+class AggregateError extends Error {}
+class TypeError extends Error {}
+class RangeError extends Error {}
+
 class ExtendedPromise extends Promise {
   static map(input) {
     return new this((resolve, reject) => {
       reject('test');
     });
   }
-  static some(input){
+  static some(input, count) {
+    let resolved = 0;
+    let rejected = 0;
+    let inputLength = 0;
+    let results = [];
+    let rejectedResults = [];
+    let finished = false;
+
     return new this((resolve, reject) => {
-      reject('test');
+      if (count < 0 || count != count) {
+        reject(new TypeError());
+        return;
+      }
+
+      Promise.resolve(input).then((iterable) => {
+        if (!Array.isArray(iterable)) {
+          reject(new TypeError());
+          return;
+        }
+        inputLength = iterable.length;
+
+        if (inputLength < count) {
+          reject(new RangeError());
+          return;
+        }
+        if (count === 0) {
+          resolve([]);
+          return;
+        }
+
+        const resolveProcessor = (value) => {
+          if (finished) return;
+          results.push(value);
+          resolved++;
+          if (resolved == count) {
+            resolve(results);
+            finished = true;
+          }
+        };
+        const rejectProcessor = (err) => {
+          if (finished) return;
+          rejected++;
+          if (inputLength - rejected <= count - resolved) {
+            reject(new AggregateError());
+            finished = true;
+          }
+        };
+        for (let value of iterable) {
+          Promise.resolve(value).then(resolveProcessor).catch(rejectProcessor);
+        }
+      }).catch(err => reject(err));
     });
   }
   static reduce(input){
@@ -15,5 +67,9 @@ class ExtendedPromise extends Promise {
     });
   }
 }
+
+ExtendedPromise.AggregateError = AggregateError;
+ExtendedPromise.TypeError = TypeError;
+ExtendedPromise.RangeError = RangeError;
 
 module.exports = ExtendedPromise;
--- week04/lec01/test/some.js
+++ week04/lec01/test/some.js
@@ -13,7 +13,7 @@ function isSubset(subset, superset) {
   }
 
   for(i = 0; i<subsetLen; i++) {
-    if(contains(superset, subset[i])) {
+    if(!contains(superset, subset[i])) {
       return false;
     }
   }
@@ -28,28 +28,32 @@ describe("Promise.some", function(){
     it("should reject on negative number", function(){
         return ExtendedPromise.some([1,2,3], -1)
             .then(assert.fail)
-            .caught(Promise.TypeError, function(){
+            .catch(function(err){
+              assert(err instanceof ExtendedPromise.TypeError);
             });
     });
 
     it("should reject on NaN", function(){
         return ExtendedPromise.some([1,2,3], -0/0)
             .then(assert.fail)
-            .caught(Promise.TypeError, function(){
+            .catch(function(err){
+              assert(err instanceof ExtendedPromise.TypeError);
             });
     });
 
     it("should reject on non-array", function(){
         return ExtendedPromise.some({}, 2)
             .then(assert.fail)
-            .caught(Promise.TypeError, function(){
+            .catch(function(err){
+              assert(err instanceof ExtendedPromise.TypeError);
             });
     });
 
     it("should reject with rangeerror when impossible to fulfill", function(){
         return ExtendedPromise.some([1,2,3], 4)
             .then(assert.fail)
-            .caught(Promise.RangeError, function(e){
+            .catch(function(err){
+              assert(err instanceof ExtendedPromise.RangeError);
             });
     });
 
@@ -65,7 +69,8 @@ var RangeError = Promise.RangeError;
 describe("Promise.some-test", function () {
 
     specify("should reject empty input", function() {
-        return ExtendedPromise.some([], 1).caught(RangeError, function() {
+        return ExtendedPromise.some([], 1).catch(function(err){
+          assert(err instanceof ExtendedPromise.RangeError);
         });
     });
 
@@ -73,7 +78,7 @@ describe("Promise.some-test", function () {
         var input = [1, 2, 3];
         return ExtendedPromise.some(input, 2).then(
             function(results) {
-                assert(testUtils.isSubset(results, input));
+                assert(isSubset(results, input));
             },
             assert.fail
         )
@@ -83,7 +88,7 @@ describe("Promise.some-test", function () {
         var input = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)];
         return ExtendedPromise.some(input, 2).then(
             function(results) {
-                assert(testUtils.isSubset(results, [1, 2, 3]));
+                assert(isSubset(results, [1, 2, 3]));
             },
             assert.fail
         )
@@ -102,40 +107,13 @@ describe("Promise.some-test", function () {
         )
     });
 
-    specify("should reject with all rejected input values if resolving howMany becomes impossible", function() {
-        var input = [Promise.resolve(1), Promise.reject(2), Promise.reject(3)];
-        return ExtendedPromise.some(input, 2).then(
-            assert.fail,
-            function(err) {
-                //Cannot use deep equality in IE8 because non-enumerable properties are not
-                //supported
-                assert(err[0] === 2);
-                assert(err[1] === 3);
-            }
-        )
-    });
-
     specify("should reject with aggregateError", function() {
         var input = [Promise.resolve(1), Promise.reject(2), Promise.reject(3)];
-        var AggregateError = Promise.AggregateError;
+        var AggregateError = ExtendedPromise.AggregateError;
         return ExtendedPromise.some(input, 2)
             .then(assert.fail)
-            .caught(AggregateError, function(e) {
-                assert(e[0] === 2);
-                assert(e[1] === 3);
-                assert(e.length === 2);
-            });
-    });
-
-    specify("aggregate error should be caught in .error", function() {
-        var input = [ExtendedPromise.resolve(1), Promise.reject(2), Promise.reject(3)];
-        var AggregateError = Promise.AggregateError;
-        return ExtendedPromise.some(input, 2)
-            .then(assert.fail)
-            .error(function(e) {
-                assert(e[0] === 2);
-                assert(e[1] === 3);
-                assert(e.length === 2);
+            .catch(function(e) {
+                assert(e instanceof AggregateError);
             });
     });
 
@@ -154,7 +132,8 @@ describe("Promise.some-test", function () {
     });
 
     specify("should reject when input promise does not resolve to array", function() {
-        return ExtendedPromise.some(Promise.resolve(1), 1).caught(TypeError, function(e){
+        return ExtendedPromise.some(Promise.resolve(1), 1).catch(function(e){
+          assert(e instanceof ExtendedPromise.TypeError);
         });
     });
 
